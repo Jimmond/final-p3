c51c4ec06a36028244622b02f2ae0e3621a3de50
Jimmond <jlortiz9903@gmail.com>
2018-11-28 00:55:15 -0400

    Update EmpleadoController.php

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -13,7 +13,7 @@ class EmpleadoController extends Controller
      */
     public function index()
     {
-        //
+        return view('empleados.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class EmpleadoController extends Controller
      */
     public function create()
     {
-        //
+       return view('empleados.create');
     }
 
     /**
